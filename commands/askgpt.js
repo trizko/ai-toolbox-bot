b26f529b653a400c3e00f2334821444e3902c1ec
Danny Rizko <drizko07@gmail.com>
2023-01-31 21:34:49 -0800

    Reformats files + Updates require statements to import

--- commands/askgpt.js
+++ commands/askgpt.js
@@ -1,36 +1,39 @@
-const axios = require("axios");
-const { SlashCommandBuilder } = require('discord.js');
-const { OPENAI_API_KEY } = require('../config.json');
-const fs = require('fs').promises;
+import axios from 'axios';
+import { SlashCommandBuilder } from 'discord.js';
+import { OPENAI_API_KEY } from '../config.json';
+import { readFile, writeFile, appendFile } from 'fs/promises';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('askgpt')
 		.setDescription('Prompt ChatGPT for answers through the OpenAI API')
-        .addBooleanOption(option =>
-            option.setName('keepcontext')
-                .setDescription('keep context from past conversation'))
-		.addStringOption(option =>
-			option.setName('prompt')
-				.setDescription('The prompt to send to ChatGPT')),
+		.addBooleanOption((option) =>
+			option
+				.setName('keepcontext')
+				.setDescription('keep context from past conversation')
+		)
+		.addStringOption((option) =>
+			option
+				.setName('prompt')
+				.setDescription('The prompt to send to ChatGPT')
+		),
 	async execute(interaction) {
 		const headers = {
 			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${OPENAI_API_KEY}`,
+			Authorization: `Bearer ${OPENAI_API_KEY}`
 		};
 		let preprompt_buf, preprompt;
-		if (interaction.options.getBoolean('keepcontext')){
-			preprompt_buf = await fs.readFile('log.txt');
-		}
-		else{
-			await fs.writeFile('log.txt', '');
+		if (interaction.options.getBoolean('keepcontext')) {
+			preprompt_buf = await readFile('log.txt');
+		} else {
+			await writeFile('log.txt', '');
 			preprompt_buf = '';
 			preprompt = '';
 		}
 		preprompt = preprompt_buf.toString('utf-8');
 		const data = {
 			model: 'text-davinci-003',
-			prompt: (preprompt + interaction.options.getString('prompt')),
+			prompt: preprompt + interaction.options.getString('prompt'),
 			temperature: 0.5,
 			max_tokens: 500,
 			top_p: 1,
@@ -39,21 +42,25 @@ module.exports = {
 			echo: false
 		};
 
-		interaction.reply("Working on it...")
+		interaction.reply('Working on it...');
 		await axios({
 			method: 'post',
 			url: 'https://api.openai.com/v1/completions',
 			data: data,
 			headers: headers
 		})
-		.then(response => {
-			const generatedText = response.data.choices[0].text;
-			interaction.editReply(interaction.options.getString('prompt') + generatedText);
-			fs.appendFile('log.txt', generatedText + '\n\n');
-		})
-		.catch(error => {
-			console.error(error);
-			interaction.editReply("An error occurred while communicating with the GPT-3 API. Please try again later.");
-		});
-	},
+			.then((response) => {
+				const generatedText = response.data.choices[0].text;
+				interaction.editReply(
+					interaction.options.getString('prompt') + generatedText
+				);
+				appendFile('log.txt', generatedText + '\n\n');
+			})
+			.catch((error) => {
+				console.error(error);
+				interaction.editReply(
+					'An error occurred while communicating with the GPT-3 API. Please try again later.'
+				);
+			});
+	}
 };
--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { DISCORD_TOKEN } = require("./config.json");
+import { readdirSync } from 'node:fs';
+import { join } from 'node:path';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { DISCORD_TOKEN } from './config.json';
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers
+	]
 });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = join(__dirname, 'commands');
+const commandFiles = readdirSync(commandsPath).filter((file) =>
+	file.endsWith('.js')
+);
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
+	const filePath = join(commandsPath, file);
 	const command = require(filePath);
+
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+		);
 	}
 }
 
-client.on("ready", () => {
-    console.log("Bot is online!");
+client.on('ready', () => {
+	console.log('Bot is online!');
 });
 
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
+		console.error(
+			`No command matching ${interaction.commandName} was found.`
+		);
 		return;
 	}
 
@@ -45,7 +52,10 @@ client.on(Events.InteractionCreate, async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		await interaction.reply({
+			content: 'There was an error while executing this command!',
+			ephemeral: true
+		});
 	}
 });
 
